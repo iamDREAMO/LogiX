48e3adbe5d324fcf9bea30666d5984879c8412a4
iamDREAMO <benedictamofah2@gmail.com>
2025-10-31 23:36:48 +0000

    gui.py: add bg to login/register buttons

--- gui.py
+++ gui.py
@@ -237,10 +237,10 @@ def create_home_screen(root):
           font=('Cambria', 20, 'bold'), justify = 'center', 
           bg="#F0F8FF", fg= "#9F00FF").pack(pady= 30)
     
-    Button(f1, text='Login', font=('Cambria', 15), fg= 'White',
-           command=lambda: create_login_screen(root)).place(x=220, y=100, width=100, height=50)
-    Button(f1, text='Register', font=('Cambria', 15), fg= 'White',
-           command=lambda: create_register_screen(root)).place(x=330, y=100, width=100, height=50)
+    Button(f1, text='Login', font=('Cambria', 15), fg= "White", bg= "#9F00FF",
+           command=lambda: create_login_screen(root)).place(x=220, y=100, width=100, height=40)
+    Button(f1, text='Register', font=('Cambria', 15), fg= "White", bg= "#9F00FF",
+           command=lambda: create_register_screen(root)).place(x=330, y=100, width=110, height=40)
 
 # ==================== Login Screen ====================
 def create_login_screen(root):
