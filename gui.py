40bc32a5854db4203219029b064d32b00cc5a07d
iamDREAMO <benedictamofah2@gmail.com>
2025-11-04 22:29:47 +0000

    register screen: add  bg "#9F00FF" to buttons

--- gui.py
+++ gui.py
@@ -312,9 +312,9 @@ def create_register_screen(root):
         except Exception as e:
             messagebox.showerror('Error', f'Failed to register: {str(e)}')
     
-    Button(f3, text='Register', font=('Cambria', 15), 
+    Button(f3, text='Register', font=('Cambria', 15),  bg="#9F00FF",
            command=perform_registration).place(x=250, y=250, width=120, height=30)
-    Button(f3, text='Home', font=('Cambria', 15), 
+    Button(f3, text='Home', font=('Cambria', 15),  bg="#9F00FF",
            command=lambda: create_home_screen(root)).place(x=20, y=350, width=100, height=30)
-    Button(f3, text='Login', font=('Cambria', 15), 
+    Button(f3, text='Login', font=('Cambria', 15),  bg="#9F00FF",
            command=lambda: create_login_screen(root)).place(x=480, y=350, width=120, height=30)
