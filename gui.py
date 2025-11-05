c4560f4134d1a1ada44ed226a6f91fc864aa5098
iamDREAMO <benedictamofah2@gmail.com>
2025-11-05 16:25:39 +0000

    search tab: add font size to label displayed results

--- gui.py
+++ gui.py
@@ -150,8 +150,8 @@ def create_search_tab(ntb):
             labels = ['Name', 'Phy', 'Chem', 'Maths']
             for i, text in enumerate(labels):
                 Label(f6, text=f"{text}: {result[i+1]}", 
-                      font=('Cambria', 12, 'bold'), bg="#F0F8FF", 
-                      fg="#9F00FF").place(x=300, y=100 + 50 * i, anchor= CENTER)
+                      font=('Cambria', 13, 'bold'), bg="#F0F8FF", 
+                      fg="#9F00FF").place(x=300, y=120 + 50 * i, anchor= CENTER)
         else:
             messagebox.showinfo('Not Found', 'Roll No. not found')
     
