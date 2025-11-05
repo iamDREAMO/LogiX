940d81ffc67f29d19f103b63d83a8cd2334794d6
iamDREAMO <benedictamofah2@gmail.com>
2025-11-05 12:16:32 +0000

    login screen: add width to 'Login' button

--- gui.py
+++ gui.py
@@ -281,7 +281,7 @@ def create_login_screen(root):
             messagebox.showerror('Login Failed', 'Invalid username or password')
     
     Button(f2, text='Login', font=('Cambria', 15), fg="White", bg="#9F00FF", justify= CENTER,
-           command=perform_login).place(x=250, y=230, width=100, height=35)
+           command=perform_login).place(x=300, y=230, width=100, height=35, anchor= CENTER)
     Button(f2, text='Home', font=('Cambria', 15), fg="White", bg="#9F00FF",
            command=lambda: create_home_screen(root)).place(x=25, y=350, width=100, height=35)
     Button(f2, text='Registration', font=('Cambria', 15), fg="White", bg="#9F00FF",
