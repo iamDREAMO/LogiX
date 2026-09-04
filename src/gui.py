34caec47242a2cb8c1566e328ec302585d803883
iamDREAMO <benedictamofah2@gmail.com>
2026-09-04 02:20:11 -0400

    Fix button text color in home screen to ensure consistency

--- src/gui.py
+++ src/gui.py
@@ -252,9 +252,9 @@ def create_home_screen(root):
           font=('Cambria', 25, 'bold'), justify = 'center', 
           bg="#F0F8FF", fg= "#9F00FF").pack(pady= 50)
     
-    Button(f1, text='Login', font=('Cambria', 15), fg= "White", bg= "#9F00FF",
+    Button(f1, text='Login', font=('Cambria', 15), fg= "white", bg= "#9F00FF",
            command=lambda: create_login_screen(root)).place(x=200, y=200, width=95, height=40)
-    Button(f1, text='Register', font=('Cambria', 15), fg= "White", bg= "#9F00FF",
+    Button(f1, text='Register', font=('Cambria', 15), fg= "white", bg= "#9F00FF",
            command=lambda: create_register_screen(root)).place(x=300, y=200, width=100, height=40)
 
 # ==================== Login Screen ====================
